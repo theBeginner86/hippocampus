281cf8fba634c5d13cac26b26c805109e5e2096a
Pranav Singh <pranavsingh02@hotmail.com>
2024-03-15 19:14:14 +0530

    add support for hget cmd
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

---
+++ handlers/hget.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"github.com/thebeginner86/hippocampus/resp"
+)
+
+func hget(args []resp.Value) resp.Value {
+	if len(args) != 2 {
+		return resp.Value{Type: "error", String: "ErrError: Invalid number of arguments for 'hget' command. Must be 2 argumentor"}
+	}
+
+	hash := args[0].Bulk
+	key := args[1].Bulk
+
+	HSETsMutex.RLock()
+	value, ok := HSETs[hash][key]
+	HSETsMutex.RUnlock()
+
+	if !ok {
+		return resp.Value{Type: "null"}
+	}
+	
+	return resp.Value{Type: "bulk", Bulk: value}
+}
