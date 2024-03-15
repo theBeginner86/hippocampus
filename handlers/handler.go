d5de52b1fb4acc3ff1a5769a7f44354acb0dda67
Pranav Singh <pranavsingh02@hotmail.com>
2024-03-15 19:14:39 +0530

    update handlers
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

--- handlers/handler.go
+++ handlers/handler.go
@@ -6,16 +6,30 @@ import (
 	"github.com/thebeginner86/hippocampus/resp"
 )
 
+// Datastructures for SETs
 // this hashmap is used to refer the key-value pairs
 var SETs = map[string]string{}
-// ReadWrite Mutext is necessary:
+// ReadWrite Mutex is necessary:
 // 1. This system is responsible for handling multiple requests concurrectly
-// 2. To ensure same key is not modified by multiple threads at the same time. That is, To ensure mutual exclusion
+// 2. To ensure same key is not modified by multiple threads at the same time. That is, to ensure mutual exclusion
 var SETsMutex = sync.RWMutex{}
 
+
+// Datastructures for HSETs
+// this nested hashmap is used to refer the key-value pairs
+var HSETs = map[string]map[string]string{}
+// ReadWrite Mutex is necessary:
+// 1. This system is responsible for handling multiple requests concurrectly
+// 2. To ensure same key is not modified by multiple threads at the same time. That is, to ensure mutual exclusion
+var HSETsMutex = sync.RWMutex{}
+
+
+// Handlers is a map of command names to their respective functions
 var Handlers = map[string]func([]resp.Value) resp.Value {
 	"PING": ping,
 	"GET": get,
 	"SET": set,
+	"HGET": hget,
+	"HSET": hset,
 }
 
