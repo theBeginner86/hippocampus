2c447571778a16ee2352a014686327a83f35d93f
Pranav Singh <pranavsingh02@hotmail.com>
2024-03-15 18:39:51 +0530

    fix main.go
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 
+	"github.com/thebeginner86/hippocampus/handlers"
 	"github.com/thebeginner86/hippocampus/resp"
 )
 
@@ -29,12 +31,37 @@ func main() {
 		respClient := resp.NewResp(conn)
 		value, err := respClient.Read()
 		if err != nil {
-			fmt.Println(err)
 			return
 		}
-		
-		_ = value
+
+		// this validaiton ensures that the request includes an array and not empty
+		if value.Type != "array" {
+			fmt.Println("Invalid request, expected array")
+			continue
+		}
+
+		if len(value.Array) == 0 {
+			fmt.Println("Invalid request, empty array")
+			continue
+		}
+
+		// we fetch the first element of the array and convert to uppercase
+		// this ensures that the cmds matches properly with our defined standards
+		command := strings.ToUpper(value.Array[0].Bulk)
+
+		// rest of the elements would be arguments of the redis cmds
+		args := value.Array[1:]
+
 		writer := resp.NewWriter(conn)
-		writer.Write(resp.Value{Type: "string", String: "OK"})
+
+		handler, ok := handlers.Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			writer.Write(resp.Value{Type: "string", String: ""})
+			continue
+		}
+
+		result := handler(args)
+		writer.Write(result)
 	}
 }
