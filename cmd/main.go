16dc84fdda12f4f64c0e3caa7365d285ab8a7166
Pranav Singh <pranavsingh02@hotmail.com>
2024-08-18 21:01:38 +0530

    update main wrt handlers signature
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -17,43 +17,34 @@ package main
 import (
 	"fmt"
 	"net"
-	"strings"
 
 	"github.com/thebeginner86/hippocampus/internal/handlers"
-	"github.com/thebeginner86/hippocampus/persistance/aof"
 	"github.com/thebeginner86/hippocampus/resp"
+)
+
+var (
+	databaseFile = "database.aof"
 )
 
 func main() {
 	fmt.Println("Listening on port :6379")
 
-	// Create a new server of type tcp listener
 	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// TODO: change it to hidden folder
-	aofH, err := aof.NewAof("database.aof")
+	handlerInst, err := handlers.NewHandler(databaseFile)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer aofH.Close()
 
-	aofH.Read(func(value resp.Value) {
-		cmd := strings.ToUpper(value.Array[0].Bulk)
-		args := value.Array[1:]
-
-		handler, ok := handlers.Handlers[cmd]
-		if !ok {
-			fmt.Println("Invalid command: ", cmd)
-			return
-		}
-
-		handler(args)
+	handlerInst.AofH.Read(func(value *resp.Value) {
+		handlerInst.ExecuteCmd(value)
 	})
+	
 
 	// Listen for connections
 	conn, err := l.Accept()
@@ -65,47 +56,25 @@ func main() {
 
 	for {
 		respClient := resp.NewResp(conn)
-		value, err := respClient.Read()
+		req, err := respClient.Read()
 		if err != nil {
 			return
 		}
 
 		// this validaiton ensures that the request includes an array and not empty
-		if value.Type != "array" {
+		if req.Type != "array" {
 			fmt.Println("Invalid request, expected array")
 			continue
 		}
 
-		if len(value.Array) == 0 {
+		if len(req.Array) == 0 {
 			fmt.Println("Invalid request, empty array")
 			continue
 		}
 
-		// fetches the first element of the array and convert to uppercase
-		// this ensures that the cmds matches properly with our defined standards
-		command := strings.ToUpper(value.Array[0].Bulk)
-
-		// rest of the elements would be arguments of the redis cmds
-		args := value.Array[1:]
-
 		writer := resp.NewWriter(conn)
 
-		handler, ok := handlers.Handlers[command]
-		// if command == "COMMAND" {continue}  
-		if !ok {
-			fmt.Println("Invalid command: ", command)
-			writer.Write(resp.Value{Type: "string", String: ""})
-			continue
-		}
-
-		if command == "SET" ||
-			command == "HSET" ||
-			(command == "CONFIG" && len(args) > 0 &&
-				strings.ToUpper(args[0].Bulk) == "SET") {
-			aofH.Write(value)
-		}
-
-		result := handler(args)
-		writer.Write(result)
+		resp := handlerInst.ExecuteCmd(req)
+		writer.Write(resp)
 	}
 }
