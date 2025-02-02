282efdff2d0aceac2324ff08e953a4f744cb199a
Pranav Singh <pranavsingh02@hotmail.com>
2025-02-02 18:25:59 +0530

    operator update
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

---
+++ operator/api/v1alpha1/hippocampus_types.go
@@ -0,0 +1,77 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// HippocampusSpec defines the desired state of Hippocampus
+type HippocampusSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// PrivateKey is the private key of the Hippocampus server. Used for secure communication between server and client.
+	PrivateKey string `json:"private_key,omitempty"`
+
+	// The following markers will use OpenAPI v3 schema to validate the value
+	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=3
+	// +kubebuilder:validation:ExclusiveMaximum=false
+
+	// Size defines the number of Memcached instances
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	Size int32 `json:"size,omitempty"`
+
+	// Port defines the port that will be used to init the container with the image
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	ContainerPort int32 `json:"containerPort,omitempty"`
+}
+
+// HippocampusStatus defines the observed state of Hippocampus
+type HippocampusStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Hippocampus is the Schema for the hippocampuses API
+type Hippocampus struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HippocampusSpec   `json:"spec,omitempty"`
+	Status HippocampusStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// HippocampusList contains a list of Hippocampus
+type HippocampusList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Hippocampus `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Hippocampus{}, &HippocampusList{})
+}
